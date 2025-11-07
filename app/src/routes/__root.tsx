afc7bde2a19b71485962242eda03a79555908da0
Anton Curanz <anton@curanz.de>
2025-11-07 21:13:46 +0100

    rm tanstack devtools

--- app/src/routes/__root.tsx
+++ app/src/routes/__root.tsx
@@ -1,6 +1,4 @@
-import { TanStackDevtools } from "@tanstack/react-devtools"
 import { Outlet, createRootRouteWithContext } from "@tanstack/react-router"
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools"
 import type { UserAccount } from "@/schema"
 import type { co } from "jazz-tools"
 import { Toaster } from "@/components/ui/sonner"
@@ -14,17 +12,6 @@ export const Route = createRootRouteWithContext<RouterContext>()({
     <div className="root">
       <Outlet />
       <Toaster />
-      <TanStackDevtools
-        config={{
-          position: "bottom-right",
-        }}
-        plugins={[
-          {
-            name: "Tanstack Router",
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
     </div>
   ),
 })
