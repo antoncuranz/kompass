223ad80ebc419d66d292711383f0b42a9bea0f47
Anton Curanz <anton@curanz.de>
2025-11-08 13:34:03 +0100

    fix docker build

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -1,19 +1,8 @@
 #!/bin/sh
+ROOT_DIR=/usr/share/nginx/html
 
-# substitute environment variables
-find /app/.next -type f -name "*.js" -print0 | while read -d $'\0' file
+# Replace env vars in files served by NGINX
+for file in $ROOT_DIR/assets/*.js* $ROOT_DIR/index.html;
 do
-  sed -i "s|VITE_MAPLIBRE_STYLE_URL_PLACEHOLDER|${MAPLIBRE_STYLE_URL}|g" $file
+  sed -i 's|VITE_MAPLIBRE_STYLE_URL_PLACEHOLDER|'${MAPLIBRE_STYLE_URL}'|g' $file
 done
-
-# docker-node entrypoint
-set -e
-
-# Run command with node if the first argument contains a "-" or is not a system command. The last
-# part inside the "{}" is a workaround for the following bug in ash/dash:
-# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=874264
-if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ] || { [ -f "${1}" ] && ! [ -x "${1}" ]; }; then
-  set -- node "$@"
-fi
-
-exec "$@"
