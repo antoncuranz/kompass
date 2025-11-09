d2114bd67514862a3d505e967b2baa8f4a64d62a
Anton Curanz <anton@curanz.de>
2025-11-09 14:11:02 +0100

    test github actions app/

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -8,7 +8,7 @@ import { JazzProvider } from "./components/provider/JazzProvider.tsx"
 import { ThemeProvider } from "./components/provider/ThemeProvider.tsx"
 import { PrivacyProvider } from "./components/provider/PrivacyProvider.tsx"
 
-// Create a new router instance
+// Create a new router instance hallo
 const router = createRouter({
   routeTree,
   defaultPreload: "intent",
