b77bc5f82db07594207b7019d3417dc6a54f2713
Anton Curanz <anton@curanz.de>
2025-11-11 00:54:34 +0100

    Fix Playwright tests (#21)
    * test github actions app/
    
    * fix playwright npx
    
    * use explicit node version
    
    * use explicit nginx version
    
    * revert test changes
    
    * verbose deps
    
    * debug
    
    * test
    
    * added custom fonts
    
    * custom fonts only for staging
    
    * same screenshots pls
    
    * speedup tests
    
    * Change npm command from ci to install
    
    * Change npm install to npm ci for dependencies
    
    * Remove 'npx -y' from jazz sync-server command
    
    * fix sync-server
    
    * race condition?
    
    * better ci config
    
    * lfs test
    
    * ci screenshots
    
    * no lfs for now

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -7,11 +7,12 @@ export const storageState = path.join(currentDir, "tests/.storageState.json")
 
 export default defineConfig({
   testDir: "./tests",
+  snapshotDir: "./tests/screenshots",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: "html",
+  reporter: process.env.CI ? [["github"], ["html"]] : "html",
   use: {
     baseURL: "http://localhost:3000",
   },
@@ -40,12 +41,16 @@ export default defineConfig({
     {
       name: "kompass app",
       command: "npm run dev -- -m staging",
+      stdout: "pipe",
       url: "http://localhost:3000",
       reuseExistingServer: !process.env.CI,
     },
     {
       name: "jazz sync-server",
-      command: "npx jazz-run sync --in-memory",
+      command: process.env.CI
+        ? "jazz-run sync --in-memory"
+        : "npx jazz-run sync --in-memory",
+      stdout: "pipe",
       url: "http://localhost:4200/health",
       reuseExistingServer: !process.env.CI,
     },
