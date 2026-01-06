5e98cdaba1a4ac980551e7321923a9eb920743f1
Anton Curanz <anton@curanz.de>
2026-01-06 17:28:15 +0100

    fixed ActivityEntry accessibility

--- app/src/components/itinerary/ActivityEntry.tsx
+++ app/src/components/itinerary/ActivityEntry.tsx
@@ -1,4 +1,5 @@
 import { useMap } from "react-map-gl/maplibre"
+import { Button } from "../ui/button"
 import JumpToMapButton from "./JumpToMapButton"
 import type { MouseEvent, MouseEventHandler } from "react"
 import type { Activity } from "@/domain"
@@ -9,7 +10,7 @@ export default function ActivityEntry({
   onClick,
 }: {
   activity: Activity
-  onClick?: MouseEventHandler<HTMLDivElement> | undefined
+  onClick?: MouseEventHandler<HTMLButtonElement> | undefined
 }) {
   const { heroMap } = useMap()
 
@@ -21,17 +22,19 @@ export default function ActivityEntry({
   }
 
   return (
-    <div
-      className="rounded-lg border border-dashed my-4 mx-5 p-2 px-3 hover:border-solid hover:shadow-sm active:shadow-xs cursor-pointer relative group/flyto"
-      onClick={onClick}
-    >
-      {activity.name}
-      <span className="float-right">
-        {activity.time && formatTime(activity.time, true)}
-      </span>
-      {activity.location && heroMap && (
-        <JumpToMapButton onClick={onChevronClick} />
-      )}
+    <div className="mx-5 my-4">
+      <Button
+        variant="secondary"
+        size="base"
+        className="w-full h-auto active:scale-[1] text-base text-left! justify-between! not-hover:shadow-none border-dashed hover:border-solid not-disabled:hover:bg-transparent relative group/flyto"
+        onClick={onClick}
+      >
+        <span>{activity.name}</span>
+        <span>{activity.time && formatTime(activity.time, true)}</span>
+        {activity.location && heroMap && (
+          <JumpToMapButton onClick={onChevronClick} />
+        )}
+      </Button>
     </div>
   )
 }
