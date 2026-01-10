a5ace0a43c4bebda71731d4cc0991ea284ee6695
Anton Curanz <anton@curanz.de>
2026-01-10 16:18:20 +0100

    improve switch styling

--- app/src/components/ui/switch.tsx
+++ app/src/components/ui/switch.tsx
@@ -7,7 +7,7 @@ function Switch({ className, children, ...props }: SwitchPrimitive.Root.Props) {
     <SwitchPrimitive.Root
       data-slot="switch"
       className={cn(
-        "peer data-[checked]:bg-primary data-[unchecked]:bg-input focus-visible:border-ring focus-visible:ring-ring/50 dark:data-[unchecked]:bg-input/80 inline-flex h-[1.15rem] w-8 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
+        "peer data-[checked]:bg-primary data-[unchecked]:bg-input focus-visible:border-ring focus-visible:ring-ring dark:data-[unchecked]:bg-input/80 inline-flex h-8 w-14 shrink-0 items-center rounded-full border shadow-sm transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 px-0.5",
         className,
       )}
       {...props}
@@ -22,7 +22,7 @@ function SwitchThumb({ className, ...props }: SwitchPrimitive.Thumb.Props) {
     <SwitchPrimitive.Thumb
       data-slot="switch-thumb"
       className={cn(
-        "bg-card dark:data-[unchecked]:bg-foreground dark:data-[checked]:bg-primary-foreground pointer-events-none block size-4 rounded-full ring-0 transition-transform data-[checked]:translate-x-[calc(100%-2px)] data-[unchecked]:translate-x-0",
+        "bg-card dark:data-[unchecked]:bg-foreground dark:data-[checked]:bg-primary-foreground pointer-events-none block size-6 rounded-full border shadow-sm ring-0 transition-transform data-[checked]:translate-x-6 data-[unchecked]:translate-x-0",
         className,
       )}
       {...props}
